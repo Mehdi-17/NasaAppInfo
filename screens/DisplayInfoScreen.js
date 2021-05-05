c3d880cf1804d963c61fd39cdf6337ab9f75cb3a
Mehdi Serra <mehdi.serra57@gmail.com>
2021-05-05 21:18:23 +0200

    debut implementation api Nasa

--- screens/DisplayInfoScreen.js
+++ screens/DisplayInfoScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+//import { REACT_NATIVE_API_KEY_NASA } from '@env';
+// TODO: voir le probleme de unimodules
 
 const DisplayInfoScreen = () => {
+    console.log('My Nasa API key = ', REACT_NATIVE_API_KEY_NASA);
     return (
         <View>
             <Text>Ecran info.</Text>
