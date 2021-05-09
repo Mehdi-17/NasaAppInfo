8f7025934983ae2c9622f18b65c49f6d7ba7c5a8
Mehdi Serra <mehdi.serra57@gmail.com>
2021-05-09 12:50:52 +0200

    last commit

--- screens/DisplayInfoScreen.js
+++ screens/DisplayInfoScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-//import { REACT_NATIVE_API_KEY_NASA } from '@env';
-// TODO: voir le probleme de unimodules
+import { REACT_NATIVE_API_KEY_NASA } from 'react-native-dotenv';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const DisplayInfoScreen = () => {
-   // console.log('My Nasa API key = ', REACT_NATIVE_API_KEY_NASA);
+    const apodApiUrl = `https://api.nasa.gov/planetary/apod?api_key=${REACT_NATIVE_API_KEY_NASA}`;
+
+    const apodApiCall = async () => {
+        const res = await axios.get(apodApiUrl);
+        console.log('res', res);
+    }
+
+    useEffect(()=>{
+        apodApiUrl();
+    }, [])
+
     return (
         <View>
-            <Text>Ecran info.</Text>
+            <Text>Ecran info. Nasa api key = {REACT_NATIVE_API_KEY_NASA}</Text>
         </View>
     );
 }
